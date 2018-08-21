32eb0cf4f1b7c4cef31727382a650f0f211b6022
scott-perretta <scottaperretta@gmail.com>
2018-08-20 20:41:38 -0400

    Refactor tickerSymbol to symbol.

--- src/main/scala/perretta/iex/client/stock/StockIEXClient.scala
+++ src/main/scala/perretta/iex/client/stock/StockIEXClient.scala
@@ -20,18 +20,18 @@ case class StockIEXClient(override protected val wsClient: StandaloneWSClient)(i
     * @return A single number, being the IEX real time price, the 15 minute
     *         delayed market price, or the previous close price, is returned.
     */
-  def getPrice(tickerSymbol: String): Future[Double] = wsClient
-    .url(baseUrl + tickerSymbol + "/price")
+  def getPrice(symbol: String): Future[Double] = wsClient
+    .url(baseUrl + symbol + "/price")
     .get()
     .map(response => Json.parse(response.body).as[Double])
 
   /**
     * Gets the location of a company's logo.
-    * @param tickerSymbol The stock's ticker symbol.
+    * @param symbol The stock's ticker symbol.
     * @return Location of the company logo.
     */
-  def getLogo(tickerSymbol: String): Future[Logo] = wsClient
-    .url(baseUrl + tickerSymbol + "/logo")
+  def getLogo(symbol: String): Future[Logo] = wsClient
+    .url(baseUrl + symbol + "/logo")
     .get()
     .map(response => Json.parse(response.body).as[Logo])
 
@@ -41,12 +41,12 @@ case class StockIEXClient(override protected val wsClient: StandaloneWSClient)(i
     * Stocks may often have a peer group associated with it. A peer group is a collection of stocks
     * that are in the same industry sector, and are of similar size, as a particular stock.
     *
-    * @param tickerSymbol The stock's ticker symbol.
+    * @param symbol The stock's ticker symbol.
     * @return An array of peer tickers as defined by IEX. This is not intended to represent
     *         a definitive or accurate list of peers, and is subject to change at any time.
     */
-  def getPeers(tickerSymbol: String): Future[Seq[String]] = wsClient
-    .url(baseUrl + tickerSymbol + "/peers")
+  def getPeers(symbol: String): Future[Seq[String]] = wsClient
+    .url(baseUrl + symbol + "/peers")
     .get()
     .map(response => Json.parse(response.body).as[Seq[String]])
 
